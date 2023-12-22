6eb52c694e30e790ceb0c18bc58e260a7e39aafc
Martín <magadiflo@gmail.com>
2023-12-22 17:49:55 -0500

    Writable and Computed Signals

--- src/app/documentation/kinds/kinds.component.ts
+++ src/app/documentation/kinds/kinds.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, WritableSignal, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-kinds',
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class KindsComponent {
+
+  public count: WritableSignal<number> = signal(0);
+  public showCount: WritableSignal<boolean> = signal(false);
+
+  public doubleCount: Signal<number> = computed(() => this.count() * 2);
+  public conditionalCount: Signal<string> = computed(() => {
+    if(this.showCount()) {
+      return `The count is ${this.count()}`;
+    } else {
+      return `Nothing to see here!`;
+    }
+  });
+
+  public setValue() {
+    this.count.set(1);
+  }
+
+  public updateValue() {
+    this.count.update(value => value + 1);
+  }
+
+  public showCountMessage() {
+    this.showCount.update(value => !value);
+  }
 
 }
