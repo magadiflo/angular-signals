77b7b0201c14524d65ed1f4e0f68bc61e6e91498
Martín <magadiflo@gmail.com>
2023-12-22 19:51:31 -0500

    RxJS Interop For Signals

--- src/app/documentation/rxjs-interop-for-signals/rxjs-interop-for-signals.component.ts
+++ src/app/documentation/rxjs-interop-for-signals/rxjs-interop-for-signals.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-interop-for-signals',
@@ -8,5 +10,8 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class RxjsInteropForSignalsComponent {
+
+  private _counterObservable: Observable<number> = interval(1000);
+  public counter: Signal<number> = toSignal(this._counterObservable, { initialValue: 0 });
 
 }
